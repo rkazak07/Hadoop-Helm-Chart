d7648e38aa89fd93f7fa3079a16837dbe4dfd525
rkazak <rkazak@BIBLP012>
2022-09-04 20:50:34 +0300

    version hadoop-3.3.3-nolib

--- image/apache-china-mirror.sh
+++
@@ -1,5 +0,0 @@
-HADOOP_30_VERSION=3.2.1
-
-curl -LO https://mirrors.huaweicloud.com/apache/hadoop/common/hadoop-$HADOOP_30_VERSION/hadoop-$HADOOP_30_VERSION-src.tar.gz
-
-curl -LO https://mirrors.huaweicloud.com/apache/hadoop/common/hadoop-$HADOOP_30_VERSION/hadoop-$HADOOP_30_VERSION.tar.gz
